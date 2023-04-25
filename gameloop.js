73aac9b00025e351faa4365506731772f433bb53
DennisNevback <dennis.nevback@gmail.com>
2023-04-25 11:16:09 +0200

    Update gameloop.js

--- gameloop.js
+++ gameloop.js
@@ -2,3 +2,4 @@ let hand1 = deck.dealHand();
 let hand2 = deck.dealHand();
 console.log('player1 hand: ' + hand1);
 player1Discard = prompt('Which cards would you like to discard?');
+let hand3 = 'fisk'
